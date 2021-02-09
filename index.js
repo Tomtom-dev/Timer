4c39bd1d45c3a4372c0c115993483a8a631733c8
Tomtom-dev <thomasheddadji0@gmail.com>
2021-02-09 11:38:24 +0100

    create class timer and start pause function test

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log("hi")
-console.log("moutarde !");
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton= startButton;
+        this.pauseButton= pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener("click", this.pause)
+    }
+
+    start(){
+        console.log('Time to start the timer');
+    }
+    pause (){
+        console.log("time to pause the program");
+    }
+}
+
+const durationInput= document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause")
+
+const timer = new Timer (durationInput, startButton, pauseButton)
